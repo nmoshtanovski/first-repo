048b047198f0faa8340d3f4868c971cbbec35fb0
nmoshtanovski <nmoshtanovski@users.noreply.github.com>
2017-01-23 17:31:58 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+#Stop on error
+#But we can't use it here because AWS CLI returns 0 or 255.
+#In the case when no stack update is required it returns 255 that is certainly not an error
+#set -e
+
+#Set environment variable
+environment=$1
 
 echo "hello, today is $(date)" > /tmp/jenkins_test
